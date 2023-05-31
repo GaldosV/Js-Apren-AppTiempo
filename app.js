2861d2cb41c79b903c61ae22f0757c0fdc2975d5
Vidal Herrero Garcia <104066977+GaldosV@users.noreply.github.com>
2023-05-31 19:31:37 +0200

    Api endpoint
    Cambio en el endpoint de la api

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             // api key 3730bad6ec5c33687f45723e87d33115
+            // endpoint - subcription plan One call api : https://openweathermap.org/api
             // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
             // {${lat}}&lon={${long}}
-            const api = `https://api.openweathermap.org/data/3.0/onecall?{${lat}}&lon={${long}}&exclude={hourly}&appid={3730bad6ec5c33687f45723e87d33115}`;
+            const key = '3730bad6ec5c33687f45723e87d33115';
+            const api = `https://api.openweathermap.org/data/3.0/onecall?{${lat}}&lon={${long}}&exclude={hourly}&appid={${key}}`;
             fetch(api)
             .then(response => {
                 return response.json();
