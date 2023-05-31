93721ce2e81ef847114af03a6b1435f333a99ebb
Vidal Herrero Garcia <104066977+GaldosV@users.noreply.github.com>
2023-05-31 19:55:46 +0200

    Añadiendo los Skycons
    Se añaden los iconos

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ window.addEventListener('load', () => {
     let long;
     let lat;
 
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector('.location-timezone');
+
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            // api key 3730bad6ec5c33687f45723e87d33115
+            // api key ************************
             // endpoint - subcription plan One call api : https://openweathermap.org/api
             // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+            // Free endpoint Solucion. https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
+            // Free endpoint http://api.openweathermap.org/data/2.5/weather?
             // {${lat}}&lon={${long}}
-            const key = '3730bad6ec5c33687f45723e87d33115';
-            const api = `https://api.openweathermap.org/data/3.0/onecall?{${lat}}&lon={${long}}&exclude={hourly}&appid={${key}}`;
+            const key = '1324';
+            const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API-KEY}`;
             fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
+                const { temperature, summary } = data.currently;
+                // Set Dom Elementos de la API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+
             });
         });
 
