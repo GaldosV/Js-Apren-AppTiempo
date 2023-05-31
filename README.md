c93c8bcb0984524fb9a48edd2b614e6703cafe11
Vidal Herrero Garcia <104066977+GaldosV@users.noreply.github.com>
2023-05-31 20:28:06 +0200

    Final Push
    push final de la App

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Js-Apren-AppTiempo
  Una App en Js para mirar el tiempo
+
+
+ Esta api mira el tiempo de tu geolocalización y te dice donde estas y la temperatura actual junto con el tiempo que hace , pulsando la pantalla cambia de celsius a farenheit .
+
+ Desgraciadamente la API de la que conseguia la información a dejado de funcionar al ser adquirida por google.
+ https://support.apple.com/en-us/HT213526
+ Así que no seguire actualizando la miniaplicación.
--- app.js
+++ app.js
@@ -5,35 +5,55 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector(".temperature-degree");
     let locationTimezone = document.querySelector('.location-timezone');
-
-
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector(".temperature span");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             // api key ************************
-            // endpoint - subcription plan One call api : https://openweathermap.org/api
-            // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-            // Free endpoint Solucion. https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
-            // Free endpoint http://api.openweathermap.org/data/2.5/weather?
-            // {${lat}}&lon={${long}}
             const key = '1324';
-            const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API-KEY}`;
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/${key}/${lat},${long}`;
             fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                const { temperature, summary } = data.currently;
+                const { temperature, summary, icon } = data.currently;
                 // Set Dom Elementos de la API
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                //Formula de farenheit - celsius
+                let celsius = (temperature - 32) * (5 / 9);
+
+                // Set icon
+                setIcons(icon, document.querySelector('.icon'));
+                // Cambiar la temperatura a celsius de farenheit
+                temperatureSection.addEventListener('click', () => { 
+                    if (temperatureSpan.textContent === "F") { 
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius); 
+                    } else {
+                        temperatureSection.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                     }
+
+                })
+                
 
             });
         });
 
       
     }
-
+    // Selector de iconos de tiempo sacados de Skycons https://github.com/darkskyapp/skycons
+    function setIcons(icon, iconID) { 
+        const skycons = new Skycon({ color: "white" });
+        // Modifica cada espacio con un underscore para que pueda recoger los datos y tranformarlos para que se recojan
+        const currentIcon = icon.replace(/-/g, "_").toUppercase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
